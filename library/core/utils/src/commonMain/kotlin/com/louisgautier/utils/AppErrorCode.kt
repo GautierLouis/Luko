e10c1633c91f98964cc887ca568606dc696496a5
Louis Gautier <Louis.gautier@outlook.fr>
2026-05-03 15:31:47 +0800

    Fix Lint

--- library/core/utils/src/commonMain/kotlin/com/louisgautier/utils/AppErrorCode.kt
+++ library/core/utils/src/commonMain/kotlin/com/louisgautier/utils/AppErrorCode.kt
@@ -1,13 +1,13 @@
 package com.louisgautier.utils
 
 sealed class AppErrorCode : Throwable() {
-    data class AppError : AppErrorCode()
+    data class AppError(override val message: String? = null) : AppErrorCode()
 
-    data class NetworkError : AppErrorCode()
+    data class NetworkError(override val message: String? = null) : AppErrorCode()
 
-    data class ServerError : AppErrorCode()
+    data class ServerError(override val message: String? = null) : AppErrorCode()
 
-    data class InvalidCredentials : AppErrorCode()
+    data class InvalidCredentials(override val message: String? = null) : AppErrorCode()
 
-    data class UnknownError : AppErrorCode()
+    data class UnknownError(override val message: String? = null) : AppErrorCode()
 }
