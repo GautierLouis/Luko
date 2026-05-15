0eb66e0128158a9271cf77c6840fe58aa236ac32
Louis Gautier <Louis.gautier@outlook.fr>
2026-05-15 16:52:05 +0800

    style

--- library/design-system/src/commonMain/kotlin/xyz/luko/designsystem/drawing/TransformStroke.kt
+++ library/design-system/src/commonMain/kotlin/xyz/luko/designsystem/drawing/TransformStroke.kt
@@ -1,6 +1,7 @@
 package xyz.luko.designsystem.drawing
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.unit.IntSize
 
 object TransformStroke {
@@ -8,12 +9,22 @@ object TransformStroke {
     private val offsetTransformer = OffsetTransformer(matrixProvider)
     private val pathTransformer = PathTransformer(matrixProvider)
 
-    fun projectToCanvas(offset: List<Offset>, canvasSize: IntSize) =
+    fun projectToCanvas(
+        offset: List<Offset>,
+        canvasSize: IntSize
+    ): List<Offset> =
         offsetTransformer.transform(offset, canvasSize)
 
-    fun unprojectFromCanvas(offset: List<Offset>, canvasSize: IntSize) =
+    fun unprojectFromCanvas(
+        offset: List<Offset>,
+        canvasSize: IntSize
+    ): List<Offset> =
         offsetTransformer.inverse(offset, canvasSize)
 
-    fun projectToCanvas(svgPathDataList: List<String>, canvasSize: IntSize, padding: Float = 0f) =
+    fun projectToCanvas(
+        svgPathDataList: List<String>,
+        canvasSize: IntSize,
+        padding: Float = 0f
+    ): List<Path> =
         pathTransformer.transform(svgPathDataList, canvasSize, padding)
 }
